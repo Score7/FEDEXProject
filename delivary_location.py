8e8180d8771f6075b71bdbf0197331b19892af22
Score7 <151198599+Score7@users.noreply.github.com>
2025-07-25 11:04:05 -0400

    Add files via upload

---
+++ delivary_location.py
@@ -0,0 +1,14 @@
+#this is a class for the location object
+
+
+#this object should be able to add a location that has the folling features
+    #a location name
+    #a location address
+    #a distance from current location
+
+class DelivaryLocation:
+    def __init__(self, locationName, locationAddress):
+        locationName = self.locationName
+        
+
+    
--- main.py
+++ main.py
@@ -1,10 +1,32 @@
-#define packages needed to display and communicate with 
+#define packages needed to display and communicate with the google maps api
 
+
+#define array that will hold user defined locations
+
+#locations will be a class
+    #location class will hold address, distance from current loaction
 
 
 
 
 
 def main():
-    #
+    #start by asking for current location
+    #stary loop that does the following
+        #ask user if they want to add a delivary location, perform the closest delivary
+        #if they choose add a delivary location
+            #call the delivary location function 
+        #if the choose closest delivary
+            #call the delivary function
+
     pass
+
+#add delivary location function
+#it wwill ask for a name and address and then call a delivary location maker function
+    #this will return to main loop
+
+#perform closest delivary function
+    #it will tell the user what the address is to drive to and what direction it is in
+    #once user enters 'done' it will delete the delivary location (or move it to completed)
+    #It will set the delivary location to the new current location
+    
